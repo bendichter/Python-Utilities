01c5c9fc06587d42a8bc7ce701c0a54c9e2c48f2
bendichter <ben.dichter@gmail.com>
2018-07-21 16:27:22 -0400

    add brpylib and setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='brpylib',
+      version='1.3.2',
+      description='accessing blackrock data in python',
+      author='Mitch Frankel',
+      packages=find_packages(),
+      install_requires=['numpy'])
